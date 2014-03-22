d8e538d645721b59b464a45369f7d0ff44a2704a
zhxrain <zhxrain@gmail.com>
2014-03-22 12:34:39 +0800

    Add nav for post admin page.

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -3,11 +3,37 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="language" content="en" />
-  {{ HTML::style("css/main.css")}}
+  <!-- Bootstrap core CSS -->
+  {{ HTML::style("css/bootstrap.css") }}
+
+  <!-- Add custom CSS here -->
+  {{ HTML::style("css/blog-home.css") }}
 </head>
 <body>
-<div class="main">
-<h1>Zhxrain</h1>
-</div>
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">管理页面</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <!-- /.navbar-collapse -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="/admin">文章</a>
+                    </li>
+                    <li><a href="/admin/editor">新建</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- /.container -->
+    </nav>
 </body>
 </html>
