2a4080cea460fb6f20c20e4ed87361cf4d9f6b10
zhxrain <zhxrain@gmail.com>
2014-06-17 21:19:20 +0800

    Fixed bug.

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -41,7 +41,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.php">Zhxrain</a>
+                <a class="navbar-brand" href="/index.php">Zhxrain</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
