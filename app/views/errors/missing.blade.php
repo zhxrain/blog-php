59a6041267843fb6ea171e750da9c28cc234a0e4
zhxrain <zhxrain@gmail.com>
2014-07-16 21:23:09 +0800

    Add 404 page.

---
+++ app/views/errors/missing.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <h1>404 Not Found.</h1>
+</body>
+</html>
